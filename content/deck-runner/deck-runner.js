233177de1790148b027e9a6eeba62e590597f502
Alan Smith <commit@alanwsmith.com>
2026-01-27 22:24:20 -0500

    working on stats for land plays

--- content/deck-runner/deck-runner.js
+++ content/deck-runner/deck-runner.js
@@ -57,7 +57,6 @@ export class DeckRunner {
     this.api.trigger("runDeck");
   }
 
-
   addCardDetails(card, index) {
     const subs = [
       ["DETAILS", this.cardStats(card, index)],
@@ -86,8 +85,14 @@ export class DeckRunner {
   }
 
   cardStats(card, index) {
+    const adjustedIndex = index - 7;
     const subs = [
-      ["TURN", index + 1],
+      ["_INDEX_", index],
+      ["_ADJUSTEDINDEX_", adjustedIndex],
+      ["TURN", adjustedIndex + 1],
+      ["TOTAL", this.totalLandsPlayedAtIndex(index)],
+      ["DRAWN", this.totalLandsAtIndex(index)],
+      ["PLAY", this.playLandAtIndex(index)],
     ];
     return this.api.makeHTML(this.templates("cardStats"), subs);
   }
@@ -108,9 +113,9 @@ export class DeckRunner {
     el.replaceChildren(
       ...this
         .deckCards()
-        .slice(7, 100)
         .map((card) => this.baseCard(card))
-        .map((card, index) => this.addCardDetails(card, index)),
+        .map((card, index) => this.addCardDetails(card, index))
+        .filter((card, index) => index >= 7),
     );
   }
 
@@ -118,6 +123,13 @@ export class DeckRunner {
     el.replaceChildren(
       ...this.deckCards().slice(0, 7).map((card) => this.cardHTML(card)),
     );
+  }
+
+  landsInOpeningHand(_, el) {
+    el.innerHTML = this.#cards
+      .slice(0, 7)
+      .map((card) => card.category() === "land" ? 1 : 0)
+      .reduce((acc, cur) => acc + cur, 0);
   }
 
   loadCards() {
@@ -165,9 +177,38 @@ export class DeckRunner {
     }
   }
 
+  playLandAtIndex(index) {
+    if (
+      this.totalLandsPlayedAtIndex(index) -
+      this.totalLandsPlayedAtIndex(index - 1)
+    ) {
+      return "Yes";
+    } else {
+      return "No";
+    }
+  }
+
   runDeck(_, el) {
     shuffleArray(this.#cards);
-    this.api.trigger("commanderSlot initialHand gameTurns");
+    this.api.trigger(`
+commanderSlot 
+gameTurns
+initialHand 
+landsInOpeningHand
+`);
+  }
+
+  totalLandsAtIndex(index) {
+    return this.#cards.slice(0, index + 1).map((card) =>
+      card.category() === "land" ? 1 : 0
+    ).reduce(
+      (acc, cur) => acc + cur,
+      0,
+    );
+  }
+
+  totalLandsPlayedAtIndex(index) {
+    return Math.min(this.totalLandsAtIndex(index), index - 6);
   }
 
   templates(kind) {
@@ -176,15 +217,19 @@ export class DeckRunner {
         return `
 <div class="card-details">
   <div>Turn: TURN</div>
+  <div>Behind: BEHIND</div>
+  <div>Adjusted Index: _ADJUSTEDINDEX_</div>
+  <div>Index: _INDEX_</div>
+  <div>Total Drawn: DRAWN</div>
   <div>Play Land: PLAY</div>
   <div>Total Played: TOTAL</div>
   <div>Remaining Lands: REMAINING</div>
-  <div>Land/Turn Delta: DELTA</div>
 </div>
 `;
       case "card":
         return `<div class="card STATUS CATEGORY">
 <img src="IMAGEURL" alt="The NAME card from Magic: The Gathering" />
+<div>xxx</div>
 </div>`;
       case "cardV2":
         return `<div class="card STATUS CATEGORY">
