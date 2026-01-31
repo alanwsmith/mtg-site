906ce6fa6df8fb27051cb8f8729b28ec6514139a
Alan Smith <commit@alanwsmith.com>
2026-01-31 00:48:32 -0500

    set up the archideckt to json converter

--- content/archidekt-converter/converter.js
+++ content/archidekt-converter/converter.js
@@ -7,27 +7,8 @@ export class Converter {
     await this.loadIdMap();
   }
 
-  acData(ev, el) {
-    const cardMatcher = /(\d+)x\s+(.*?)\s+\(.*?\[(\w+)/;
-    this._cards = [];
-    ev.value.split("\n")
-      .map((line) => line.match(cardMatcher))
-      .filter((match) => match !== null)
-      .filter((match) => match[3] !== undefined)
-      .filter((match) => match[3] !== "Maybeboard")
-      .filter((match) => match[3] !== "Sideboard")
-      .forEach((match, index) => {
-        for (let count = 0; count < parseInt(match[1], 10); count += 1) {
-          this.#data.cards.push(
-            {
-              name: match[2],
-              id: this.#idMap[match[2]],
-              kind: match[3].toLowerCase(),
-            },
-          );
-        }
-      });
-    el.value = JSON.stringify(this.#data);
+  clearList(_, el) {
+    el.value = "";
   }
 
   async loadIdMap() {
@@ -39,5 +20,38 @@ export class Converter {
     } else {
       this.#errors.push(response.error);
     }
+  }
+
+  showExample(_, el) {
+    const text = `1x Abjure (wth) 31 [Removal]
+1x Aboshan, Cephalid Emperor (plst) ODY-58 [Protection]
+1x Academy Ruins (drc) 58 [Land]
+1x Access Tunnel (tdc) 337 [Land]
+1x Accursed Witch // Infectious Curse (soi) 97 [Ramp]
+1x Adaptive Omnitool (drc) 16 [Draw]
+1x Aether Poisoner (aer) 51 [Tokens]`;
+    el.value = text;
+    this.api.trigger("outputJSON");
+  }
+
+  outputJSON(_, el) {
+    this.#data.cards = [];
+    const cardMatcher = /(\d+)x\s+(.*?)\s+\(.*?\[(\w+)/;
+    document.querySelector(".input-data").value.split("\n")
+      .map((line) => line.match(cardMatcher))
+      .filter((match) => match !== null)
+      .filter((match) => match[3] !== undefined)
+      .forEach((match, index) => {
+        for (let count = 0; count < parseInt(match[1], 10); count += 1) {
+          this.#data.cards.push(
+            {
+              name: match[2],
+              id: this.#idMap[match[2]],
+              kind: match[3].toLowerCase(),
+            },
+          );
+        }
+      });
+    el.value = JSON.stringify(this.#data.cards);
   }
 }
