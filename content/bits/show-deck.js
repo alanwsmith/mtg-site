fb9e4c8a75761dd12b0c94c3122052b97c2c45c4
Alan Smith <commit@alanwsmith.com>
2026-01-31 18:09:53 -0500

    updated convert to use number istead of new card each time for repeated cards

--- content/bits/show-deck.js
+++ content/bits/show-deck.js
@@ -1,4 +1,10 @@
 const t = {
+  commander: `
+<div class="commander">
+  <div class="deck-section-title">Commander</div>
+  <div class="deck-section">CARDS</div>
+</div>`,
+
   card: `<div class="card">
   <img 
     alt="The NAME card from Magic: The Gathering" 
@@ -8,19 +14,45 @@ const t = {
 };
 
 export class ShowDeck {
-  async showDeck(ev, el) {
-    const response = await this.api.getJSON(el.prop("deck"));
-    if (response.value) {
-      el.replaceChildren(
-        ...response.value.cards
-          .filter((card) => card.kind !== "mabybeboard")
-          .map((card) => {
-            const subs = [["ID", card.id]];
-            return this.api.makeHTML(t.card, subs);
-          }),
-      );
+  #cards;
+
+  deck(_, el) {
+    el.replaceChildren(this.getCards("commander"));
+  }
+
+  getCards(kind) {
+    const cards = this.#cards
+      .filter((card) => card.kind === kind)
+      .map((card) => {
+        const cardSubs = [
+          ["ID", card.id],
+          ["NAME", card.name],
+        ];
+        return this.api.makeHTML(t.card, cardSubs);
+      });
+    const subs = [
+      ["CARDS", cards],
+    ];
+    return this.api.makeHTML(t[kind], subs);
+  }
+
+  async loadDeck(ev, el) {
+    const resp = await this.api.getJSON(el.prop("deck"));
+    if (resp.value) {
+      this.#cards = resp.value.cards;
+      console.log(this.#cards);
+
+      // el.replaceChildren(
+      //   ...response.value.cards
+      //     .filter((card) => card.kind !== "mabybeboard")
+      //     .map((card) => {
+      //       const subs = [["ID", card.id]];
+      //       return this.api.makeHTML(t.card, subs);
+      //     }),
+      // );
     } else {
       el.innerHTML = "Error fetching JSON";
     }
+    this.api.trigger("deck");
   }
 }
