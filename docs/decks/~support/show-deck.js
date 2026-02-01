cba3c36690b1e2d81d6bb62e0be74c3770e6debb
Alan Smith <commit@alanwsmith.com>
2026-01-31 22:22:55 -0500

    basic showing card in place

--- docs/decks/~support/show-deck.js
+++ docs/decks/~support/show-deck.js
@@ -2,15 +2,16 @@ const t = {
   section: `
 <div class="deck-section-wrapper KIND-section">
   <div class="deck-section-title">KIND</div>
-  <div class="deck-section">CARDS</div>
+  <div class="deck-section-cards">CARDS</div>
 </div>`,
 
-  card: `<div class="card">
+  card: `<div class="card" data-send="showCard" data-id="ID">
   <img 
     alt="The NAME card from Magic: The Gathering" 
     src="/images/cards/ID.jpg">
 </div>
 `,
+  displayCard: `<img src="/images/cards/ID.jpg" alt="Current Display Card" />`,
 };
 
 export class ShowDeck {
@@ -26,28 +27,22 @@ export class ShowDeck {
   getCards(kind) {
     const cards = this.#cards
       .filter((card) => card.kind === kind)
-      .map((card) => {
+      .map((card, index) => {
         const cardSubs = [
           ["ID", card.id],
           ["NAME", card.name],
         ];
-        return this.api.makeHTML(t.card, cardSubs);
+        const cardEl = this.api.makeElement(t.card, cardSubs);
+        cardEl.style.setProperty("--card-index", index);
+        return cardEl;
       });
     const subs = [
       ["KIND", kind],
       ["CARDS", cards],
     ];
-    return this.api.makeHTML(t.section, subs);
-  }
-
-  sectionKinds() {
-    return [
-      ...new Set(
-        this.#cards.filter((card) => card.kind !== "commander").map((card) =>
-          card.kind
-        ),
-      ),
-    ].sort();
+    const deckSectionWrapper = this.api.makeElement(t.section, subs);
+    deckSectionWrapper.style.setProperty("--wrapper-bump", cards.length);
+    return deckSectionWrapper;
   }
 
   async loadDeck(ev, el) {
@@ -55,18 +50,27 @@ export class ShowDeck {
     if (resp.value) {
       this.#cards = resp.value.cards;
       console.log(this.#cards);
-
-      // el.replaceChildren(
-      //   ...response.value.cards
-      //     .filter((card) => card.kind !== "mabybeboard")
-      //     .map((card) => {
-      //       const subs = [["ID", card.id]];
-      //       return this.api.makeHTML(t.card, subs);
-      //     }),
-      // );
     } else {
       el.innerHTML = "Error fetching JSON";
     }
     this.api.trigger("deck");
+  }
+
+  sectionKinds() {
+    return [
+      ...new Set(
+        this.#cards
+          .filter((card) => card.kind !== "commander")
+          .filter((card) => card.kind !== "maybeboard")
+          .map((card) => card.kind),
+      ),
+    ].sort();
+  }
+
+  showCard(ev, el) {
+    const subs = [
+      ["ID", ev.prop("id")],
+    ];
+    el.replaceChildren(this.api.makeHTML(t.displayCard, subs));
   }
 }
