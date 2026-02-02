95355a381a5edfde5e25ee484e76bd891be067a6
Alan Smith <commit@alanwsmith.com>
2026-02-02 13:32:26 -0500

    displaying the commander

--- content/deck-refiner/~support/deck-refiner.js
+++ content/deck-refiner/~support/deck-refiner.js
@@ -1,19 +1,15 @@
 const t = {
-  section: `
-<div class="deck-section-wrapper KIND-section">
+  card: `<div class="card" data-send="showCard" data-id="ID">IMAGE_TAG</div>`,
+
+  scryfallImageURL: `<img 
+src="https://cards.scryfall.io/normal/front/CHAR1/CHAR2/ID.jpg?HASH"
+alt="ALT"
+data-id="ID" />`,
+
+  section: `<div class="deck-section-wrapper KIND-section">
   <div class="deck-section-title">KIND</div>
   <div class="deck-section-cards">CARDS</div>
 </div>`,
-
-  card: `<div class="card" data-send="showCard" data-id="ID">
-  <img 
-    alt="The NAME card from Magic: The Gathering" 
-    src="/images/cards/ID.jpg"
-    data-id="ID"
-  />
-</div>
-`,
-  displayCard: `<img src="/images/cards/ID.jpg" alt="Current Display Card" />`,
 };
 
 export class DeckRefiner {
@@ -27,8 +23,58 @@ export class DeckRefiner {
     });
   }
 
+  cardsInCategory(category) {
+    return this.#deck.cards
+      .filter((card) => card.categories[0] === category);
+  }
+
+  cardName(card) {
+    return card.card.oracleCard.name;
+  }
+
   commander(_, el) {
-    el.innerHTML = "Commander";
+    //el.replaceChildren(this.api.makeHTML(t.displayCard))
+
+    // el.innerHTML = this.#deck.cards
+    //   .filter((card) => card.categories[0] === "Commander")
+    //   .map((card) => card.card.oracleCard.name);
+
+    // const subs = this.#deck.cards
+    //   .filter((card) => card.categories[0] === "Commander")
+    //   .map(
+    //     (card) => {
+    //       return [["NAME", card.card.oracleCard.name], ["ID", card.card.uid], [
+    //         "IMAGE_URL",
+    //         this.scryfallImageURL(card),
+    //       ]];
+    //     },
+    //   );
+
+    const card = this.cardsInCategory("Commander")[0];
+    const subs = [
+      ["NAME", this.cardName(card)],
+      ["IMAGE_TAG", this.scryfallImageTag(card)],
+    ];
+
+    console.log(subs);
+
+    el.replaceChildren(
+      this.api.makeHTML(
+        t.card,
+        subs,
+      ),
+    );
+
+    // const id = this.#deck.cards
+    //   .filter((card) => card.categories[0] === "Commander")
+    //   .map((card) => card.card.uid);
+    // const subs = [
+    //   ["ID", id],
+    // ];
+
+    //el.replaceChildren(this.api.makeHTML(t.displayCard, subs));
+
+    //el.innerHTML = id;
   }
 
   deck(_, el) {
@@ -45,6 +91,20 @@ export class DeckRefiner {
     } else {
       console.log(resp.error);
     }
+  }
+
+  scryfallImageTag(card) {
+    // `https://cards.scryfall.io/normal/front/f/e/fe9be3e0-076c-4703-9750-2a6b0a178bc9.jpg?1761053654`;
+    return this.api.makeTXT(
+      t.scryfallImageURL,
+      [
+        ["ID", card.card.uid],
+        ["CHAR1", card.card.uid.substring(0, 1)],
+        ["CHAR2", card.card.uid.substring(1, 2)],
+        ["HASH", card.card.scryfallImageHash],
+        ["NAME", this.cardName(card)],
+      ],
+    );
   }
 
   update(event) {}
